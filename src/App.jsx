e3592c09fc998def1874f43e63c44b61dc1e6041
Eduardo Alvarez <el_alienx@hotmail.com>
2021-08-16 10:24:43 +0200

    Add a quick selection in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-// import MyApp from "./1-readable/good/App";
-// import MyApp from "./2-reusable/good/App";
-import MyApp from "./3-refactorable/good/App";
+import ReadableBad from "./1-readable/bad/App";
+import ReadableGood from "./1-readable/good/App";
+import ReusableBad from "./2-reusable/bad/App";
+import ReusableGood from "./2-reusable/good/App";
+import RefactorableBad from "./3-refactorable/bad/App";
+import RefactorableGood from "./3-refactorable/good/App";
 
 export default function App() {
+  /**
+   * Instructions:
+   * Comment all components and only leave the one you want to view.
+   */
   return (
     <div className="App">
-      <MyApp />
+      {/* --- Category: Refadable good and bad */}
+      <ReadableBad />
+      {/* <ReadableGood /> */}
+
+      {/* --- Category: Refadable good and bad */}
+      {/* <ReusableBad/>
+      <ReusableGood/> */}
+
+      {/* --- Category: Refactorable good and bad */}
+      {/* <RefactorableBad/> */}
+      {/* <RefactorableGood/> */}
     </div>
   );
 }
