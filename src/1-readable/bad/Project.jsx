2f3b637801cd11dd5af8bd9bc08278e27632dae4
Eduardo <el_alienx@hotmail.com>
2022-02-23 20:44:23 +0100

    added a warning in 1-bad Project.jsx

--- src/1-readable/bad/Project.jsx
+++ src/1-readable/bad/Project.jsx
@@ -1,69 +1,88 @@
+/**
+ * Note: 
+ * This file is exluded from Prettier on purpose to compare the lack of formatting.
+ * To see the difference, create a new branch and remove Project.jsx from .prettierignore
+ */
+
 // Project
-function Pr() {
-  const c = "usd"; // currency
-  const i = [
-    // inventory
+function Pr(){
+  const c = 'usd' // currency
+  const i =[ // inventory
     {
-      product: "Flashlight",
-      img: "/flashlight.jpg",
+      product: 'Flashlight',
+      img: '/flashlight.jpg',
       desc: "A really great flashlight",
       price: 100,
       id: 1,
-      c: "usd",
+      c: 'usd'
     },
     {
-      product: "Tin can",
-      img: "/tin_can.jpg",
+      product: 'Tin can',
+      img: '/tin_can.jpg',
       desc: "Pretty much what you would expect from a tin can",
       price: 32,
       id: 2,
-      c: "usd",
+      c: 'usd'
     },
     {
-      product: "Cardboard Box",
-      img: "/cardboard_box.png",
+      product: 'Cardboard Box',
+      img: '/cardboard_box.png',
       desc: "It holds things",
       price: 5,
       id: 3,
-      c: "usd",
-    },
-  ];
+      c: 'usd'
+    }
+  ]
 
   return (
     <>
-      <h2>1 Bad</h2>
-      <p>Global currency: {c}</p>
+    <h2>1 Bad</h2>
+    <p>Global currency: {c}</p>
 
-      <table style={{ width: "100%" }}>
-        <tbody>
-          <tr>
-            <th>Product</th>
+    <table style={{width: '100%'}}>
+    <tbody>
+    <tr>
+    <th>
+      Product
+    </th>
 
-            <th>Image</th>
+    <th>
+      Image
+    </th>
 
-            <th>Description</th>
+    <th>
+      Description
+    </th>
 
-            <th>Price</th>
+    <th>
+      Price
+    </th>
+    </tr>
+      {i.map(function(i, idx) {
+        return (
+          <tr key = {idx}>
+            <td>
+              {i.product}
+            </td>
+
+            <td>
+            <img src={i.img} alt=""/>
+            </td>
+
+            <td>
+            {i.desc}
+            </td>
+
+            <td>
+              {i.price}
+            </td>
           </tr>
-          {i.map(function (i, idx) {
-            return (
-              <tr key={idx}>
-                <td>{i.product}</td>
-
-                <td>
-                  <img src={i.img} alt="" />
-                </td>
-
-                <td>{i.desc}</td>
-
-                <td>{i.price}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
+        );
+      })}
+  </tbody>
+  </table>
+  </>
   );
 }
 
-export default Pr;
+export default Inv;
