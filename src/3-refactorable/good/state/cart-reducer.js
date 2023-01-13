70fdb4cbbec555e7ab9e4208e266f0765f59e664
Eduardo <el_alienx@hotmail.com>
2023-01-13 14:11:50 +0100

    some typo refactor

--- src/3-refactorable/good/state/cart-reducer.js
+++ src/3-refactorable/good/state/cart-reducer.js
@@ -13,8 +13,8 @@ export default function cartReducer(state, action) {
 // Functional programming.
 /**
  * Seems like an overkill here (and it is)
- * But image if we need 10 lines of code to add a cart.
- * Example validating if the itemId exist in our inventory.
+ * But imagine if we need 10 lines of code to add to the cart.
+ * Example: validating if the itemId still is in stock.
  * Then it becomes easier to read, outside the switch
  */
 function add(state, action) {
